7b06135130d26af9304f23d8edf09a0fac3c126c
Fabio Campos <jfcampos@prodam.sp.gov.br>
2023-06-07 10:26:22 -0300

    ajuste erros

--- flex_exp/lib/main.dart
+++ flex_exp/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,34 +12,36 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text('Flexible & Expanded'),
+        title: const Text('Flexible & Expanded'),
       ),
       body: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
           Container(
             height: 100,
-            child: Text('Item 1 - pretty big!'),
             color: Colors.red,
+            child: const Text('Item 1 - pretty big!'),
           ),
           Expanded(
             flex: 4,
             child: Container(
               height: 100,
-              child: Text('Item 2'),
               color: Colors.blue,
+              child: const Text('Item 2'),
             ),
           ),
           Flexible(
@@ -45,8 +49,8 @@ class MyHomePage extends StatelessWidget {
             flex: 2,
             child: Container(
               height: 100,
-              child: Text('Item 3'),
               color: Colors.orange,
+              child: const Text('Item 3'),
             ),
           ),
         ],
