64aecc108d4cd1e60d8e2765ccfc657f42d23fbd
fabio <campos_fabio@hotmail.com>
2023-07-02 12:48:45 -0300

    Section 9 - final

--- shop/lib/utils/app_routes.dart
+++ shop/lib/utils/app_routes.dart
@@ -3,4 +3,6 @@ class AppRoutes {
   static const String productDetail = '/product-detail';
   static const String cart = '/cart';
   static const String orders = '/orders';
+  static const String products = '/products';
+  static const String productForm = '/product_form';
 }
