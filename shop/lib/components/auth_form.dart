93bfd51ebe8ff468b647d7c36b9688c3cd50571c
fabio <campos_fabio@hotmail.com>
2023-07-10 14:03:56 -0300

    Section 12 - 317

--- shop/lib/components/auth_form.dart
+++ shop/lib/components/auth_form.dart
@@ -12,9 +12,38 @@ class AuthForm extends StatefulWidget {
   State<AuthForm> createState() => _AuthFormState();
 }
 
-class _AuthFormState extends State<AuthForm> {
+class _AuthFormState extends State<AuthForm>
+    with SingleTickerProviderStateMixin {
   final _passwordController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+
+  AnimationController? _controller;
+  Animation<Size>? _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+
+    _heightAnimation = Tween(
+      begin: const Size(double.infinity, 310),
+      end: const Size(double.infinity, 400),
+    ).animate(CurvedAnimation(
+      parent: _controller!,
+      curve: Curves.linear,
+    ));
+
+    //_heightAnimation?.addListener(() => setState(() {}));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller?.dispose();
+  }
 
   bool _isLoading = false;
 
@@ -32,8 +61,10 @@ class _AuthFormState extends State<AuthForm> {
     setState(() {
       if (_isLogin()) {
         _authMode = AuthMode.signUp;
+        _controller?.forward();
       } else {
         _authMode = AuthMode.login;
+        _controller?.reverse();
       }
     });
   }
@@ -97,88 +128,93 @@ class _AuthFormState extends State<AuthForm> {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
       ),
-      child: Container(
-          height: _isLogin() ? 310 : 400,
+      child: AnimatedBuilder(
+        animation: _heightAnimation!,
+        builder: (ctx, child) => Container(
+          //height: _isLogin() ? 310 : 400,
+          height: _heightAnimation?.value.height ?? (_isLogin() ? 310 : 400),
           width: deviceSize.width * 0.75,
           padding: const EdgeInsets.all(16),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'E-mail'),
-                  keyboardType: TextInputType.emailAddress,
-                  onSaved: (email) => _authData['email'] = email ?? '',
-                  validator: (email) {
-                    email = email ?? '';
+          child: child,
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'E-mail'),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (email) => _authData['email'] = email ?? '',
+                validator: (email) {
+                  email = email ?? '';
 
-                    if (email.trim().isEmpty || !email.contains('@')) {
-                      return 'Informe um e-mail válido!';
-                    }
-                    return null;
-                  },
-                ),
+                  if (email.trim().isEmpty || !email.contains('@')) {
+                    return 'Informe um e-mail válido!';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Senha'),
+                keyboardType: TextInputType.emailAddress,
+                obscureText: true,
+                controller: _passwordController,
+                onSaved: (password) => _authData['password'] = password ?? '',
+                validator: (password) {
+                  password = password ?? '';
+
+                  if (password.isEmpty || password.length < 5) {
+                    return 'Informe uma senha válida!';
+                  }
+                  return null;
+                },
+              ),
+              if (_isSignup())
                 TextFormField(
-                  decoration: const InputDecoration(labelText: 'Senha'),
+                  decoration:
+                      const InputDecoration(labelText: 'Confirmar Senha'),
                   keyboardType: TextInputType.emailAddress,
                   obscureText: true,
-                  controller: _passwordController,
-                  onSaved: (password) => _authData['password'] = password ?? '',
-                  validator: (password) {
-                    password = password ?? '';
+                  validator: _isLogin()
+                      ? null
+                      : (password) {
+                          password = password ?? '';
 
-                    if (password.isEmpty || password.length < 5) {
-                      return 'Informe uma senha válida!';
-                    }
-                    return null;
-                  },
+                          if (password != _passwordController.text) {
+                            return 'Senhas informadas não conferem';
+                          }
+                          return null;
+                        },
                 ),
-                if (_isSignup())
-                  TextFormField(
-                    decoration:
-                        const InputDecoration(labelText: 'Confirmar Senha'),
-                    keyboardType: TextInputType.emailAddress,
-                    obscureText: true,
-                    validator: _isLogin()
-                        ? null
-                        : (password) {
-                            password = password ?? '';
-
-                            if (password != _passwordController.text) {
-                              return 'Senhas informadas não conferem';
-                            }
-                            return null;
-                          },
-                  ),
-                const SizedBox(
-                  height: 20,
-                ),
-                _isLoading
-                    ? const CircularProgressIndicator()
-                    : ElevatedButton(
-                        onPressed: _submit,
-                        style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30),
-                            ),
-                            backgroundColor:
-                                Theme.of(context).colorScheme.primary,
-                            foregroundColor:
-                                Theme.of(context).colorScheme.background,
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 30, vertical: 8)),
-                        child: Text(_authMode == AuthMode.login
-                            ? 'ENTRAR'
-                            : 'REGISTRAR'),
-                      ),
-                const Spacer(),
-                TextButton(
-                    onPressed: _switchAuthMode,
-                    child: Text(
-                        _isLogin() ? 'DESEJA REGISTRAR?' : 'JÁ POSSUI CONTA?')),
-              ],
-            ),
-          )),
+              const SizedBox(
+                height: 20,
+              ),
+              _isLoading
+                  ? const CircularProgressIndicator()
+                  : ElevatedButton(
+                      onPressed: _submit,
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primary,
+                          foregroundColor:
+                              Theme.of(context).colorScheme.background,
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 30, vertical: 8)),
+                      child: Text(
+                          _authMode == AuthMode.login ? 'ENTRAR' : 'REGISTRAR'),
+                    ),
+              const Spacer(),
+              TextButton(
+                  onPressed: _switchAuthMode,
+                  child: Text(
+                      _isLogin() ? 'DESEJA REGISTRAR?' : 'JÁ POSSUI CONTA?')),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
